3c20ea78b44e5d0b1bc90022e1613d3ea555b30d
Shravani bhadale <shravnicloud@gmail.com>
2026-07-08 11:07:27 +0530

    Fix echo statement formatting in jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -5,4 +5,4 @@ echo "This script creates a folder and a main.txt file."
 mkdir -p jenkins_2
 touch jenkins_2/main.txt
 
-echo "Folder 'jenkins_2' and file 'main.txt' created .
+echo "Folder 'jenkins_2' and file 'main.txt' created "
