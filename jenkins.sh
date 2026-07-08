e15d5a7f4b7b60378eaed80816193b06fd4b6064
Shravani bhadale <shravnicloud@gmail.com>
2026-07-08 10:38:26 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -2,7 +2,7 @@
 
 echo "This script creates a folder and a main.txt file."
 
-mkdir -p jenkins_1
-touch jenkins_1/main.txt
+mkdir -p jenkins
+touch jenkins/main.txt
 
 echo "Folder 'jenkins_1' and file 'main.txt' created successfully."
